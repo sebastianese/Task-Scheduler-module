d0259159c2ce2eef2a1cba3e6500f9872c5ff241
Sebastianese <sebastianese@gmail.com>
2017-03-14 00:38:40 -0700

    tested1

---
+++ UpdateTask/UpdateTask.psm1
@@ -0,0 +1,49 @@
+﻿function Add-UpdateTask {
+    <#
+            .SYNOPSIS
+            This module is designed to schedule Windows Update tasks on our enviroment. 
+You will be asked for the administration IP and for the date to schedule the patching Task. 
+Please make sure to use the same date format as on the example  
+            .Example
+            Add-UpdateTasK -IP 172.0.0.0
+            .PARAMETER IPAddress
+            Enter IP Address.
+            .PARAMETER Date
+            Enter Date for patching (Example: 11/28/2016 3:30PM).
+        
+            #>
+ 
+        [cmdletBinding()]  
+        param(
+            [Parameter(Mandatory=$True, ValueFromPipeline=$True)]
+            [string]$IPAddress,
+            [string]$Date
+            )
+    
+    
+    Invoke-Command -cn $IPAddress   -Credential "" -ScriptBlock {
+    Unregister-ScheduledTask -TaskName "Windows Update" -Confirm:$false -ErrorAction Ignore
+    $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-command "Import-Module -Name PSWindowsUpdate ; Get-WUInstall  –AcceptAll –AutoReboot"'
+
+    $Date=  Read-Host -Prompt 'Enter Date for patching (Example: 11/28/2016 3:30PM).'
+    $question1=  Read-Host -Prompt 'Boss, Do you want to set the task to run 2 times in case than an update fails? Plase type Yes or No.'
+    
+    #$trigger =  New-ScheduledTaskTrigger -Once -At "11/28/2016 3PM"
+   if ($question1 -eq "Yes") {
+    $Date2=  Read-Host -Prompt 'Enter Date for the Secondadry Patching Task (Example: 11/28/2016 3:30PM).'
+    $trigger =  New-ScheduledTaskTrigger -Once -At $Date
+    $trigger2 =  New-ScheduledTaskTrigger -Once -At $Date2
+    
+    Register-ScheduledTask -Action $action -Trigger $trigger,$trigger2 -TaskName "Windows Update" -Description "Run updates from WSUS and reboot once completed" -RunLevel Highest -User "NT AUTHORITY\SYSTEM"
+    Write-Output "Scheduling 2 tasks on $IPAddress"
+    }
+    else {
+    $trigger =  New-ScheduledTaskTrigger -Once -At $Date
+    
+    
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Windows Update" -Description "Run updates from WSUS and reboot once completed" -RunLevel Highest -User "NT AUTHORITY\SYSTEM"
+    Write-Output "Scheduling 1 task on $IPAddress"
+    }
+    Write-Output "Success!!"
+    }
+}
---
+++ UpdateTask2008/UpdateTask2008.psm1
@@ -0,0 +1,66 @@
+﻿function Add-UpdateTask2008 {
+    <#
+            .SYNOPSIS
+            Creates Windows update schedule task  
+            .Example
+            Get-TaskCreated -computername localhost
+            .PARAMETER IPAddress
+            Enter IP Address.
+        
+            #>
+        [cmdletBinding()]
+        param(
+            [Parameter(Mandatory=$True, ValueFromPipeline=$True)]
+            [string]$IPAddress
+            )
+    
+
+
+
+    Invoke-Command -cn $IPAddress  -Credential "" -ScriptBlock {
+    # The name of the scheduled task
+    $TaskName = "Windows Update"
+    # The description of the task
+    $TaskDescr = "Download and install WSUS updates. Automatically reboot system"
+    # The Task Action command
+    $TaskCommand = "Powershell.exe"
+    # The PowerShell script to be executed
+    $TaskScript = '-command "Import-Module -Name PSWindowsUpdate ; Get-WUInstall  –AcceptAll –AutoReboot"'
+    # The Task Action command argument
+    $TaskArg = "$TaskScript"
+
+    # The time when the task starts, for demonstration purposes we run it 1 minute after we created the task
+    #$TaskStartTime = $([char]34)+$Date+"T"+$Time+$([char]34)
+
+ 
+
+
+    # attach the Task Scheduler com object
+    $service = new-object -ComObject("Schedule.Service")
+    # connect to the local machine. 
+    # http://msdn.microsoft.com/en-us/library/windows/desktop/aa381833(v=vs.85).aspx
+    $service.Connect()
+    $rootFolder = $service.GetFolder("\")
+
+    $TaskDefinition = $service.NewTask(0) 
+    $TaskDefinition.RegistrationInfo.Description = "$TaskDescr"
+    $TaskDefinition.Settings.Enabled = $true
+    $TaskDefinition.Settings.AllowDemandStart = $true
+
+    $triggers = $TaskDefinition.Triggers
+    #http://msdn.microsoft.com/en-us/library/windows/desktop/aa383915(v=vs.85).aspx
+    $trigger = $triggers.Create(1) # Creates a "One time" trigger
+    #$trigger.StartBoundary = $TaskStartTime.ToString("yyyy-MM-dd'T'HH:mm:ss")
+    #$trigger.StartBoundary = "2016-10-28T15:35:00"
+    $trigger.StartBoundary =  Read-Host -Prompt 'Enter date and time for task on format yyyy-MM-ddTHH:mm:ss (Example: 2016-10-28T09:35:00 )'
+    $trigger.Enabled = $true
+
+    # http://msdn.microsoft.com/en-us/library/windows/desktop/aa381841(v=vs.85).aspx
+    $Action = $TaskDefinition.Actions.Create(0)
+    $action.Path = "$TaskCommand"
+    $action.Arguments = "$TaskArg"
+
+    #http://msdn.microsoft.com/en-us/library/windows/desktop/aa381365(v=vs.85).aspx
+    $rootFolder.RegisterTaskDefinition("$TaskName",$TaskDefinition,6,"System",$null,5)
+    }
+}
